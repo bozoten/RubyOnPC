ab6d3df0f87e3b8ec7ba93c43d1dd771d82402e2
bozoten <shridharapavel@gmail.com>
2024-10-01 16:20:41 -0400

    Made a simple GUI

--- main.py
+++ main.py
@@ -7,45 +7,68 @@ $ pip install google-generativeai
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
+import tkinter as tk
+from tkinter import messagebox
 import subprocess
-
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
 
-py_env_path = "destination.py"
+py_env_path = "target.py"
 
-# Create the model
+systemPrompt = "Do not return any formatting such as markdown, quotes, or backticks. Provide only the raw Python script as plain text."
+
+  # Create the model
 generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 64,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+  }
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash",
-  generation_config=generation_config,
-)
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+  )
 
-chat_session = model.start_chat(
-  history=[
-  ]
-)
+def do_task(prompt):
+  chat_session = model.start_chat(
+    history=[
+    ]
+  )
 
-response = chat_session.send_message("Do not return any formatting such as markdown, quotes, or backticks. Provide only the raw Python script USERS PROMPT: Hey gemini can you generate a python script that will write a tex file for a mock resume with clean format and then convert the tex file into docx using pandoc?")
+  response = chat_session.send_message(systemPrompt + prompt)
+  response = response.text.strip('```')  
+  with open(py_env_path, 'w') as file:
+      file.write(response)
+      print(response)
 
-response = str(response.text)
+def gui():
+  prompt = prompt_text.get("1.0", tk.END).strip()
 
-with open(py_env_path, 'w') as file:
-    file.write(response)
+  if prompt:
+     do_task(prompt=prompt)
+     command = ['python', py_env_path]    
+
+     subprocess.run(command, check=True)
+     messagebox.showinfo("Success", "Task Executed!")
+     prompt_text.delete("1.0", tk.END)  
+  else:
+     messagebox.showwarning("Input Error", "Please enter a prompt.")  
+  
+root = tk.Tk()
+root.title("RubyOnPC")
+
+prompt_text = tk.Text(root, height=5, width=30)
+prompt_text.pack(pady=10)
+
+save_button = tk.Button(root, text="Execute", command=gui)
+save_button.pack(pady=5)
+
+root.mainloop()
 
 
-command = ['python', py_env_path]    
 
-subprocess.run(command, check=True)
-
-print(response)
 
